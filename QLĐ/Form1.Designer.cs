f04efb6ebdaf9fddd40e11df2d8c70afb8100a1f
Dotronghuy <140100028+Dotronghuy@users.noreply.github.com>
2024-04-25 22:04:04 +0700

    add file

---
+++ QLĐ/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿
+namespace QLĐ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tbMaMH = new System.Windows.Forms.TextBox();
+            this.tbSotiet = new System.Windows.Forms.TextBox();
+            this.txTenMH = new System.Windows.Forms.TextBox();
+            this.txtMaMH = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTenMH = new System.Windows.Forms.Label();
+            this.txtSoTiet = new System.Windows.Forms.Label();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(90, 206);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(667, 232);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // tbMaMH
+            // 
+            this.tbMaMH.Location = new System.Drawing.Point(90, 12);
+            this.tbMaMH.Name = "tbMaMH";
+            this.tbMaMH.Size = new System.Drawing.Size(667, 22);
+            this.tbMaMH.TabIndex = 2;
+            // 
+            // tbSotiet
+            // 
+            this.tbSotiet.Location = new System.Drawing.Point(90, 113);
+            this.tbSotiet.Name = "tbSotiet";
+            this.tbSotiet.Size = new System.Drawing.Size(667, 22);
+            this.tbSotiet.TabIndex = 3;
+            // 
+            // txTenMH
+            // 
+            this.txTenMH.Location = new System.Drawing.Point(90, 59);
+            this.txTenMH.Name = "txTenMH";
+            this.txTenMH.Size = new System.Drawing.Size(667, 22);
+            this.txTenMH.TabIndex = 4;
+            // 
+            // txtMaMH
+            // 
+            this.txtMaMH.AutoSize = true;
+            this.txtMaMH.Location = new System.Drawing.Point(12, 17);
+            this.txtMaMH.Name = "txtMaMH";
+            this.txtMaMH.Size = new System.Drawing.Size(52, 17);
+            this.txtMaMH.TabIndex = 5;
+            this.txtMaMH.Text = "Mã MH";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 17);
+            this.label2.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 17);
+            this.label3.TabIndex = 7;
+            // 
+            // txtTenMH
+            // 
+            this.txtTenMH.AutoSize = true;
+            this.txtTenMH.Location = new System.Drawing.Point(12, 64);
+            this.txtTenMH.Name = "txtTenMH";
+            this.txtTenMH.Size = new System.Drawing.Size(58, 17);
+            this.txtTenMH.TabIndex = 8;
+            this.txtTenMH.Text = "Tên MH";
+            // 
+            // txtSoTiet
+            // 
+            this.txtSoTiet.AutoSize = true;
+            this.txtSoTiet.Location = new System.Drawing.Point(11, 118);
+            this.txtSoTiet.Name = "txtSoTiet";
+            this.txtSoTiet.Size = new System.Drawing.Size(53, 17);
+            this.txtSoTiet.TabIndex = 9;
+            this.txtSoTiet.Text = "Số Tiết";
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(121, 157);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(75, 33);
+            this.btnThem.TabIndex = 10;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // btnSua
+            // 
+            this.btnSua.Location = new System.Drawing.Point(219, 157);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(75, 33);
+            this.btnSua.TabIndex = 11;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.UseVisualStyleBackColor = true;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(321, 157);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(75, 33);
+            this.btnXoa.TabIndex = 12;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(420, 157);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(75, 33);
+            this.btnThoat.TabIndex = 13;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnSua);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.txtSoTiet);
+            this.Controls.Add(this.txtTenMH);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtMaMH);
+            this.Controls.Add(this.txTenMH);
+            this.Controls.Add(this.tbSotiet);
+            this.Controls.Add(this.tbMaMH);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Cập Nhập Môn Học";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox tbMaMH;
+        private System.Windows.Forms.TextBox tbSotiet;
+        private System.Windows.Forms.TextBox txTenMH;
+        private System.Windows.Forms.Label txtMaMH;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label txtTenMH;
+        private System.Windows.Forms.Label txtSoTiet;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnThoat;
+    }
+}
+
---
+++ QLĐ/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace QLĐ
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection ketnoiCSDL()
+        {
+            string strCon = "Data Source=LAPTOP-29UG1RC2\\SQL;Initial Catalog=QLDiem;Integrated Security=True";
+            SqlConnection con = new SqlConnection(strCon);
+            con.Open();
+            return con;
+        }
+        void loadData()
+        {
+            using (SqlConnection con = ketnoiCSDL())
+            {
+                SqlCommand cmd = new SqlCommand("SELECT * FROM Monhoc", con);
+                SqlDataReader dr = cmd.ExecuteReader();
+                DataTable dt = new DataTable();
+                dt.Load(dr);
+                dataGridView1.DataSource = dt;
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            loadData();
+        }
+
+
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection con = ketnoiCSDL())
+            {
+                string sql = "INSERT INTO MonHoc VALUES (@MaMH, @TenMH, @Sotiet)";
+                SqlCommand cmd = new SqlCommand(sql, con);
+                cmd.Parameters.AddWithValue("@MaMH", tbMaMH.Text);
+                cmd.Parameters.AddWithValue("@TenMH", txTenMH.Text);
+                cmd.Parameters.AddWithValue("@Sotiet", tbSotiet.Text);
+
+                cmd.ExecuteNonQuery();
+            }
+            loadData();
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection con = ketnoiCSDL())
+            {
+                string sql = "UPDATE MonHoc SET TenMH = @tenmh, Sotiet = @sotiet WHERE MaMH = @mamh";
+                SqlCommand cmd = new SqlCommand(sql, con);
+                cmd.Parameters.AddWithValue("@tenmh", txTenMH.Text);
+                cmd.Parameters.AddWithValue("@sotiet", tbSotiet.Text);
+                cmd.Parameters.AddWithValue("@mamh", tbMaMH.Text);
+                cmd.ExecuteNonQuery();
+            }
+            loadData();
+        }
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int rowIndex = e.RowIndex;
+            if (rowIndex >= 0 && rowIndex < dataGridView1.RowCount - 1)
+            {
+                tbMaMH.Text = dataGridView1.Rows[rowIndex].Cells[0].Value.ToString();
+                txTenMH.Text = dataGridView1.Rows[rowIndex].Cells[1].Value.ToString();
+                tbSotiet.Text = dataGridView1.Rows[rowIndex].Cells[2].Value.ToString();
+            }
+            else
+            {
+                txTenMH.Clear();
+                tbMaMH.Clear();
+                tbSotiet.Clear();
+            }
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                DialogResult result = MessageBox.Show("Bạn có chắc chắn muốn xóa dòng này không?", "Xác nhận xóa", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (result == DialogResult.Yes)
+                {
+                    string maMH = dataGridView1.SelectedRows[0].Cells[0].Value.ToString();
+                    DeleteMonHoc(maMH);
+                    loadData(); 
+                }
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng chọn dòng để xóa.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void DeleteMonHoc(string maMH)
+        {
+            using (SqlConnection con = ketnoiCSDL())
+            {
+                string sql = "DELETE FROM MonHoc WHERE MaMH = @mamh";
+                SqlCommand cmd = new SqlCommand(sql, con);
+                cmd.Parameters.AddWithValue("@mamh", maMH);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Bạn có chắc chắn muốn thoát khỏi ứng dụng không?", "Xác nhận thoát", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit(); 
+            }
+        }
+
+    }
+}
